a198db54468048dd3888c1a90eab4ef0e1650373
karkirowle <szerk.animeblog@gmail.com>
2017-12-20 15:34:55 +0100

    polynomial questions

--- VanillaID/exp20171123/twogenenet.m
+++ VanillaID/exp20171123/twogenenet.m
@@ -1,6 +1,5 @@
 % Two gene net for reconstruction method
-% Aim: Gene 1 represses Gene 2
-
+% Aim: Gene 1 represses Gene 
 function  [w_ours] = twogenenet(state, which)
 % state - the number of measurement sets to to use for predicting 
 % (a number greater  equal than 1)
@@ -12,13 +11,13 @@ samplingRate = 1;
 timePoints = 100;
 noise = 0.01;
 lambda = 0.1;
-MAXITER = 5;
+MAXITER = 10;
 realState = 2;
 h = 4;
 initialConditions = [20,10,10];
 
 % Interpretation
-timeLag = 2;
+timeLag = 4;
 
 
 % ------------------------- Initial conditions ----------------------------
@@ -42,7 +41,7 @@ disp("End of initial X");
 % ------------------------- Simulation graph ------------------------------
 
 % Simulation settings
-simulation = simulationGraph(2,4,0);
+simulation = simulationGraph(2,4,0,1,false);
 simulation = simulation.degradation(1,0.2);
 simulation = simulation.degradation(2,0.2);
 simulation = simulation.repression(1,2, 0.5, 4);
@@ -51,20 +50,20 @@ w_tru = simulation.weightMatrix;
 functionNumber = size(w_tru,1);
 
 % Interpretation graph -> expecting state many nodes with order of 4
-interpretation = simulationGraph(state,4,timeLag);
+interpretation = simulationGraph(state,4,timeLag,5,true);
 
 
 % ------------------------- Euler simulation ------------------------------
 
 for k=(2+timeLag):timePoints
     % Simulation dictionary functions
-    Phi(k-1,:) = simulation.getNextDictionary(X(k-1,:), false);
+    Phi(k-1,:) = simulation.getNextDictionary(X(k-1,:));
     % feed 20 10 10 in
     % Identification dictionary functions
     % Uses only the number observed
     % feed 20 20 10 two times in, but one gene so that 20;20
     Phi2(k-1,:) = interpretation.getNextDictionary(X((k-1-timeLag) ...
-        :(k-1),1:state), true);
+        :(k-1),1:state));
 
     % Generate next time step of X
     X(k,1:realState)=X(k-1,1:realState) + ...
@@ -83,7 +82,6 @@ for k=(2+timeLag):timePoints
 end
 
 % ------------------------- Reconstruction --------------------------------
-
 w_ours =  tac_reconstruction(Y(:,which), Phi2, lambda,MAXITER);
 disp(w_ours);
 
@@ -94,6 +92,9 @@ title('Reconstructed motif of two-gene toy example')
 xlabel('Increasing concentration of gene 1')
 ylabel('Concentration effect on itself');
 box off;
+
+figure(2)
+plot(Y);
 
 end
 
